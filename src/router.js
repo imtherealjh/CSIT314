49022011772b878fa1e7a209426b047857cc3288
Ng Jun Hua <jhng032@mymail.sim.edu.sg>
2022-11-05 07:51:12 +0800

    Fixed submit-review page ui

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const userRoute = require("./routes/userRoute");
 const userModel = require("./models/userModel");
 
 router.get("/", (req, res) => {
-    res.render("reviewers-submit-review");
+    res.render("submit-review");
 });
 
 router.get("/admin/create/user", (req, res) => {
