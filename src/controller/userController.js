5eb40d913acd6b217ac4dfb47c79d9485872dd9f
SaiLaoKhayKham <sailkk001@mymail.sim.edu.sg>
2022-11-04 10:36:45 +0800

    testing2

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -5,7 +5,7 @@ module.exports = {
         await conn.promise().execute("CREATE TABLE USER(id int PRIMARY KEY auto_increment,username varchar(45), password varchar(45))")
         let result = await conn.promise().query("SELECT * FROM USER");
         conn.end();
-        console.log("Hello")
+        console.log("Hello2")
         return res.send(result[0])
     },
 };
